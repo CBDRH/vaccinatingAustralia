ed726c6d03d765ba7b5188f8f11e618700560f15
Mark Hanly <m.hanly@unsw.edu.au>
2021-03-09 10:17:55 +1100

    updated unitsMax parameter and tweaked the gap between first and second dose

--- R/runProjections.R
+++ R/runProjections.R
@@ -28,10 +28,10 @@ hesitancy13 <- c(0, 0, 0, 0,
 
 # Scenario 1
 sc1 <- vaccinateAustralia(
-        units = 80000,
+        unitsMax = 80000,
         startDate = day1,
         gapStart = 3*7,
-        gapEnd = 6*7,
+        gapEnd = 4*7,
         hesitancy = hesitancy7
 )
 save(sc1, file = 'Outputs/sc1.rda')
@@ -40,10 +40,10 @@ beepr::beep(7)
 
 # Scenario 2
 sc2 <- vaccinateAustralia(
-    units = 80000,
+    unitsMax = 80000,
     startDate = day1,
     gapStart = 3*7,
-    gapEnd = 6*7,
+    gapEnd = 4*7,
     hesitancy = hesitancy13
 )
 save(sc2, file = 'Outputs/sc2.rda')
@@ -52,10 +52,10 @@ beepr::beep(7)
 
 # Scenario 3
 sc3 <- vaccinateAustralia(
-    units = 80000,
+    unitsMax = 80000,
     startDate = day1,
-    gapStart = 3*7,
-    gapEnd = 12*7,
+    gapStart = 12*7,
+    gapEnd = 13*7,
     hesitancy = hesitancy7
 )
 save(sc3, file = 'Outputs/sc3.rda')
@@ -64,10 +64,10 @@ beepr::beep(7)
 
 # Scenario 4
 sc4 <- vaccinateAustralia(
-    units = 80000,
+    unitsMax = 80000,
     startDate = day1,
-    gapStart = 3*7,
-    gapEnd = 12*7,
+    gapStart = 12*7,
+    gapEnd = 13*7,
     hesitancy = hesitancy13
 )
 save(sc4, file = 'Outputs/sc4.rda')
@@ -76,10 +76,10 @@ beepr::beep(7)
 
 # Scenario 5
 sc5 <- vaccinateAustralia(
-    units = 180000,
+    unitsMax = 200000,
     startDate = day1,
     gapStart = 3*7,
-    gapEnd = 6*7,
+    gapEnd = 4*7,
     hesitancy = hesitancy7
 )
 save(sc5, file = 'Outputs/sc5.rda')
@@ -88,10 +88,10 @@ beepr::beep(7)
 
 # Scenario 6
 sc6 <- vaccinateAustralia(
-    units = 200000,
+    unitsMax = 200000,
     startDate = day1,
     gapStart = 3*7,
-    gapEnd = 6*7,
+    gapEnd = 4*7,
     hesitancy = hesitancy13
 )
 save(sc6, file = 'Outputs/sc6.rda')
@@ -100,10 +100,10 @@ beepr::beep(7)
 
 # Scenario 7
 sc7 <- vaccinateAustralia(
-    units = 200000,
+    unitsMax = 200000,
     startDate = day1,
-    gapStart = 3*7,
-    gapEnd = 12*7,
+    gapStart = 12*7,
+    gapEnd = 13*7,
     hesitancy = hesitancy7
 )
 save(sc7, file = 'Outputs/sc7.rda')
@@ -112,22 +112,22 @@ beepr::beep(7)
 
 # Scenario 8
 sc8 <- vaccinateAustralia(
-    units = 200000,
+    unitsMax = 200000,
     startDate = day1,
-    gapStart = 3*7,
-    gapEnd = 12*7,
+    gapStart = 12*7,
+    gapEnd = 13*7,
     hesitancy = hesitancy13
 )
 save(sc8, file = 'Outputs/sc8.rda')
 beepr::beep(7)
 
 ## Check results
-check_results(sc1, units = 80000, gapEnd = 6*7, gapStart = 3*7)
-check_results(sc2, units = 80000, gapEnd = 6*7, gapStart = 3*7)
-check_results(sc3, units = 80000, gapEnd = 12*7, gapStart = 3*7)
-check_results(sc4, units = 80000, gapEnd = 12*7, gapStart = 3*7)
-check_results(sc5, units = 200000, gapEnd = 6*7, gapStart = 3*7)
-check_results(sc6, units = 200000, gapEnd = 6*7, gapStart = 3*7)
-check_results(sc7, units = 200000, gapEnd = 12*7, gapStart = 3*7)
-check_results(sc8, units = 200000, gapEnd = 12*7, gapStart = 3*7)
+check_results(sc1, units = 80000, gapEnd = 3*7, gapStart = 4*7)
+check_results(sc2, units = 80000, gapEnd = 3*7, gapStart = 4*7)
+check_results(sc3, units = 80000, gapEnd = 12*7, gapStart = 13*7)
+check_results(sc4, units = 80000, gapEnd = 12*7, gapStart = 13*7)
+check_results(sc5, units = 200000, gapEnd = 3*7, gapStart = 4*7)
+check_results(sc6, units = 200000, gapEnd = 3*7, gapStart = 4*7)
+check_results(sc7, units = 200000, gapEnd = 12*7, gapStart = 13*7)
+check_results(sc8, units = 200000, gapEnd = 12*7, gapStart = 13*7)
 beepr::beep(8)
